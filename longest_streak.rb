f2e7b1779015e61bc4cd82c4399b2882481e2c08
Blake Erickson <o.blakeerickson@gmail.com>
2013-11-25 07:40:49 -0700

    added some parameters to methods user_list and last_user

--- longest_streak.rb
+++ longest_streak.rb
@@ -9,17 +9,29 @@ class Connection
   end
   def username
     @client.login
-    # client.user 'oblakeerickson'
-    # response = client.last_response
-    # etag = response.headers
-    # etag[:'x-ratelimit-remaining']
   end
 
   def rate_limit
     @client.user 'oblakeerickson'
     response = @client.last_response
-    rate_limit = response.headers[:'x-ratelimit-remaining'].to_i
+    @rate_limit = response.headers[:'x-ratelimit-remaining'].to_i
   end
+
+  def user_list(since)
+    @list = @client.all_users :since => since
+  end
+
+  def last_user(list)
+    list.last.id
+  end
+
+  # def get_all_users
+  #   while @rate_limit > 10 do 
+  #     list = user_list last
+  #     last = last_user list
+  #     @rate_limit = rate_limit
+  #   end
+  # end
 
   private
     def get_username
@@ -31,3 +43,15 @@ class Connection
       File.read(password_file).chomp
     end
 end
+
+# @c = Connection.new
+# list = @c.user_list 135
+# last = @c.last_user list
+# last = 0
+# rate_limit = @c.rate_limit
+# while rate_limit > 10 do
+#   list = @c.user_list last
+#   last = @c.last_user list
+#   rate_limit = @c.rate_limit
+#   puts "last: #{last} | rate limit: #{rate_limit}"
+# end
--- spec/longest_streak_spec.rb
+++ spec/longest_streak_spec.rb
@@ -10,4 +10,13 @@ describe Connection do
     @connection.rate_limit.should be_kind_of(Integer)
   end
 
+  it "should be a list of users" do
+    @connection.user_list(0).should be_kind_of Array
+  end
+
+  it "should return last user id" do
+    list = @connection.user_list(0)
+    @connection.last_user(list).should be_kind_of(Integer)
+  end
+
 end
