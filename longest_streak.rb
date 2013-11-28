34fce6f70df813bf0d1e73657e33b3dc74dbe1ba
Blake Erickson <o.blakeerickson@gmail.com>
2013-11-28 09:33:13 -0700

    created User class and can save data in database

--- longest_streak.rb
+++ longest_streak.rb
@@ -1,5 +1,7 @@
 require 'Octokit'
 require 'netrc'
+require 'sequel'
+DB = Sequel.connect('sqlite://streak.db')
 
 class Connection
   def initialize
@@ -9,6 +11,10 @@ class Connection
   end
   def username
     @client.login
+  end
+
+  def user(username)
+    @client.user username
   end
 
   def rate_limit
@@ -43,6 +49,46 @@ class Connection
       File.read(password_file).chomp
     end
 end
+
+# get a chunk of users
+# for each user insert or update them
+# repeat
+
+class User
+  def initialize(user)
+    @id = user.id
+    @login = user.login
+  end
+  def id
+    @id
+  end
+  def login
+    @login
+  end
+  def print
+    puts "id: #{id}"
+    puts "login: #{login}"
+  end
+  def save
+    DB.run("insert into user (id, login) values('#{id}', '#{login}')")
+  end
+end
+
+@connection = Connection.new
+list = @connection.user_list 0
+user = list.first
+my_user = User.new(user)
+my_user.print
+my_user.save
+puts user.gravatar_id
+puts user.followers
+
+#DB.run("insert into user (id, login) values('#{user.id}', '#{user.login}')")
+
+
+# @c = Connection.new
+# user = @c.user 'oblakeerickson'
+# puts user.email
 
 # @c = Connection.new
 # list = @c.user_list 135
